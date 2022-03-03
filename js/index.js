667eafc9c7f987bee98c90947113abe5af27b59a
Ayus Irfang Filaras <61302674+wadahkode@users.noreply.github.com>
2022-03-03 17:20:16 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Story Card
+ * 
+ * How to create feature like, dislike and comment
+ * 
+ * @author wadahkode <mvp.dedefilaras@gmail.com>
+ * @github https://github.com/wadahkode/wadahkode
+ * @since version 0.0.1
+ */
+(async (root) => storyComponent({root, data: await sampleRequest()}))(document.getElementById('root'))
---
+++ js/request.js
@@ -0,0 +1,54 @@
+let example = new Blob([JSON.stringify([
+  {
+    id: 1,
+    title: "Title 1",
+    name: "wadahkode",
+    story: "Blabla",
+    liked: [],
+    comments: []
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    name: "wadahkode",
+    story: "Blibli",
+    liked: [],
+    comments: []
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    name: "wadahkode",
+    story: "Blublu",
+    liked: [],
+    comments: []
+  },
+  {
+    id: 4,
+    title: "Title 4",
+    name: "wadahkode",
+    story: "Bleble",
+    liked: [],
+    comments: []
+  },
+  {
+    id: 5,
+    title: "Title 5",
+    name: "wadahkode",
+    story: "Bloblo",
+    liked: [],
+    comments: []
+  }
+], null, 2)], {
+  type: "application/json"
+})
+
+const sampleRequest = () => {
+  const reader = new FileReader()
+  
+  return new Promise((resolve, reject) => {
+    reader.onload = (e) => resolve(JSON.parse(e.target.result))
+    reader.readAsText(example)
+  })
+  //return JSON.parse(await (new Response(example)).text())
+}
